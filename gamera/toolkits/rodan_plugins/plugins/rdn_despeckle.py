b42033cf323bc4c7df8da7b5e24104c34229dbf4
Anton Khelou <a.khelou@gmail.com>
2013-01-31 16:30:24 -0500

    Add rdn_despeckle function wrapper

---
+++ gamera/toolkits/rodan_plugins/plugins/rdn_despeckle.py
@@ -0,0 +1,33 @@
+from gamera.plugin import PluginFunction, PluginModule
+from gamera.args import ImageType, Args, Int
+from gamera.enums import ONEBIT
+
+
+class rdn_despeckle(PluginFunction):
+    """
+        A thin wrapper around despeckle() to get the
+        desired despeckling behaviour for Rodan.
+
+        Desired behaviour involves returning a gamera Image, rather
+        just applying it to the image that is passed in and returning nothing.
+    """
+    pure_python = 1
+    self_type = ImageType(ONEBIT)
+    return_type = ImageType(ONEBIT)
+    args = Args([Int('cc_size', range=(1, 100))])
+
+    def __call__(self, cc_size):
+        self.despeckle(cc_size)
+        return self
+
+    __call__ = staticmethod(__call__)
+
+
+class RodanDespecklePluginGenerator(PluginModule):
+    category = "despeckling"
+    cpp_headers = []
+    functions = [rdn_despeckle]
+    author = "Anton Khelou"
+    url = "http://ddmal.music.mcgill.ca"
+
+module = RodanDespecklePluginGenerator()
